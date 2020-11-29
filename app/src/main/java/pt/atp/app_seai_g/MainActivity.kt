fd253f2a491da0f4e70c5f0de530edd5bec52726
Inês Soares <up201606615@fe.up.pt>
2020-11-29 17:09:50 +0000

    Confirm password added to register

--- app/src/main/java/pt/atp/app_seai_g/MainActivity.kt
+++ app/src/main/java/pt/atp/app_seai_g/MainActivity.kt
@@ -1,22 +1,12 @@
 package pt.atp.app_seai_g
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.PorterDuff
-import android.os.Build
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.MotionEvent
 import android.widget.Button
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.drawable.DrawableCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ClickableViewAccessibility")
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,19 +20,6 @@ class MainActivity : AppCompatActivity() {
         button_register.setOnClickListener{
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val mUsername: String = email.text.toString().trim()
-        val mPassword: String = password.text.toString().trim()
-        val t = mUsername.isNotEmpty() && mPassword.isNotEmpty()
-        if (t) {
-            button.setBackgroundResource(R.color.colorPrimaryDark)
-        }
-        else {
-            button.setBackgroundResource(R.color.colorwhitegreenshade)
         }
     }
 }
