4831c88c9850111fccb98d87fa287053b998f183
Inês Soares <up201606615@fe.up.pt>
2020-11-29 14:47:17 +0000

    Charging activity

--- app/src/main/java/pt/atp/app_seai_g/ChargingMode.kt
+++ app/src/main/java/pt/atp/app_seai_g/ChargingMode.kt
@@ -1,8 +1,10 @@
 package pt.atp.app_seai_g
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_charging_mode.*
+import kotlinx.android.synthetic.main.activity_id_carregador.*
 
 class ChargingMode : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,18 @@ class ChargingMode : AppCompatActivity() {
 
         val bb: Bundle? = intent.extras
         chargerid.text = bb!!.getString("chargerID")
+
+        chargenormal.setOnClickListener{
+            //TODO send info to control: charging mode NORMAL
+            val intent = Intent(this, Charging::class.java)
+            startActivity(intent)
+        }
+
+        chargefast.setOnClickListener{
+            //TODO send info to control: charging mode FAST
+            val intent = Intent(this, Charging::class.java)
+            startActivity(intent)
+        }
 
     }
 }
