9987d645c11564c39bac31ae743c49f9b3a493b4
Inês Soares <up201606615@fe.up.pt>
2020-12-07 23:05:51 +0000

    nullPointerException when trying to access the button

--- app/src/main/java/pt/atp/app_seai_g/Welcome.kt
+++ app/src/main/java/pt/atp/app_seai_g/Welcome.kt
@@ -1,9 +1,15 @@
 package pt.atp.app_seai_g
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureActivity
 
 class Welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +31,45 @@ class Welcome : AppCompatActivity() {
             true
         }
 
+        val buttonSendId = findViewById<Button>(R.id.button_sendid)
+        buttonSendId.setOnClickListener{
+            val intent = Intent(this,ChargingMode::class.java)
+            val idCharger = findViewById<EditText>(R.id.idcarregador)
+            intent.putExtra("chargerID",idCharger.text.toString())
+            startActivity(intent)
+        }
+
+        val buttonQrCode = findViewById<Button>(R.id.button_qrcode)
+        buttonQrCode.setOnClickListener{
+            scanQRCode()
+        }
+
+        val buttonNFC = findViewById<Button>(R.id.button_nfc)
+        buttonNFC.setOnClickListener{
+            val intent = Intent(this, ReceiverActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun scanQRCode(){
+        val integrator = IntentIntegrator(this).apply{
+            captureActivity= CaptureActivity::class.java
+            setOrientationLocked(false)
+            setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+            setPrompt("Scanning Code")
+        }
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode,data)
+        if(result!=null){
+            if(result.contents == null) Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show()
+            else Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+        } else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 
     private fun setCurrentFragment(fragment:Fragment)=
