62ca46338738475f1aa6bf2cc2dd54077cb41350
Inês Soares <up201606615@fe.up.pt>
2020-12-07 18:56:04 +0000

    Register works

--- app/src/main/java/pt/atp/app_seai_g/Register.kt
+++ app/src/main/java/pt/atp/app_seai_g/Register.kt
@@ -12,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
 
+    private var nameTV: EditText? = null
     private var emailTV: EditText? = null
     private var passwordTV: EditText? = null
     private var confirmPasswordTV: EditText? = null
@@ -30,9 +31,15 @@ class Register : AppCompatActivity() {
     }
 
     private fun registerNewUser() {
+        val name: String = nameTV!!.text.toString()
         val email: String = emailTV!!.text.toString()
         val password: String = passwordTV!!.text.toString()
         val confirmPassword: String = confirmPasswordTV!!.text.toString()
+
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(applicationContext, "Please enter name", Toast.LENGTH_LONG).show()
+            return
+        }
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(applicationContext, "Please enter email", Toast.LENGTH_LONG).show()
             return
@@ -45,6 +52,7 @@ class Register : AppCompatActivity() {
             Toast.makeText(applicationContext, "Passwords don't match", Toast.LENGTH_LONG).show()
             return
         }
+
         mAuth!!.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
@@ -52,8 +60,7 @@ class Register : AppCompatActivity() {
                         applicationContext,
                         "Registration successful!",
                         Toast.LENGTH_LONG
-                    )
-                        .show()
+                    ).show()
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                 } else {
@@ -67,6 +74,7 @@ class Register : AppCompatActivity() {
     }
 
     private fun initializeUI() {
+        nameTV = findViewById(R.id.name)
         emailTV = findViewById(R.id.email)
         passwordTV = findViewById(R.id.password)
         confirmPasswordTV = findViewById(R.id.confirm_password)
