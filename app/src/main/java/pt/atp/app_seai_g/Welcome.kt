9aeea35f84090ccd40c77b6c7525949015aac40a
Inês Soares <up201606615@fe.up.pt>
2020-12-08 21:05:57 +0000

    Deleted comment in Welcome.kt

--- app/src/main/java/pt/atp/app_seai_g/Welcome.kt
+++ app/src/main/java/pt/atp/app_seai_g/Welcome.kt
@@ -30,47 +30,7 @@ class Welcome : AppCompatActivity() {
             }
             true
         }
-
-        /*val buttonSendId = findViewById<Button>(R.id.button_sendid2)
-        buttonSendId.setOnClickListener{
-            val intent = Intent(this,ChargingMode::class.java)
-            val idCharger = findViewById<EditText>(R.id.idcarregador2)
-            intent.putExtra("chargerID",idCharger.text.toString())
-            startActivity(intent)
-        }
-
-        val buttonQrCode = findViewById<Button>(R.id.button_qrcode2)
-        buttonQrCode.setOnClickListener{
-            scanQRCode()
-        }
-
-        val buttonNFC = findViewById<Button>(R.id.button_nfc2)
-        buttonNFC.setOnClickListener{
-            val intent = Intent(this, ReceiverActivity::class.java)
-            startActivity(intent)
-        }*/
-
     }
-
-    /*private fun scanQRCode(){
-        val integrator = IntentIntegrator(this).apply{
-            captureActivity= CaptureActivity::class.java
-            setOrientationLocked(false)
-            setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
-            setPrompt("Scanning Code")
-        }
-        integrator.initiateScan()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode,data)
-        if(result!=null){
-            if(result.contents == null) Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show()
-            else Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-        } else{
-            super.onActivityResult(requestCode, resultCode, data)
-        }
-    }*/
 
     private fun setCurrentFragment(fragment:Fragment)=
         supportFragmentManager.beginTransaction().apply {
