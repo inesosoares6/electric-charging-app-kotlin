d565872a6ee7f0d9451ef5d975b59117af835b47
Inês Soares <up201606615@fe.up.pt>
2020-12-08 21:04:39 +0000

    deleted unused classes

--- app/src/main/java/pt/atp/app_seai_g/ChargeFragment.kt
+++ app/src/main/java/pt/atp/app_seai_g/ChargeFragment.kt
@@ -7,8 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureActivity
 
 
 class ChargeFragment : Fragment(R.layout.fragment_charge) {
@@ -16,14 +17,31 @@ class ChargeFragment : Fragment(R.layout.fragment_charge) {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val rootView: View = inflater.inflate(R.layout.fragment_charge,container,false)
-        val buttonSendId: Button = rootView.findViewById(R.id.button_sendid2)
+        val buttonSendId: Button = rootView.findViewById(R.id.button_sendid)
+//        val buttonQrCode: Button = rootView.findViewById(R.id.button_qrcode)
+//        val buttonNFC: Button = rootView.findViewById(R.id.button_nfc)
 
         buttonSendId.setOnClickListener {
             val intent = Intent(context,ChargingMode::class.java)
-            val chargerIdText: EditText = rootView.findViewById(R.id.idcarregador2)
+            val chargerIdText: EditText = rootView.findViewById(R.id.idcarregador)
             intent.putExtra("chargerID",chargerIdText.text.toString())
             startActivity(intent)
         }
+
+/*        buttonQrCode.setOnClickListener{
+            val integrator = IntentIntegrator(activity).apply{
+                captureActivity= CaptureActivity::class.java
+                setOrientationLocked(false)
+                setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+                setPrompt("Scanning Code")
+            }
+            integrator.initiateScan()
+        }
+
+        buttonNFC.setOnClickListener{
+            val intent = Intent(context, ReceiverActivity::class.java)
+            startActivity(intent)
+        }*/
 
         return rootView
     }
