5e9fc84b73ffd9f878f2924ef3faadd6e58512d5
Inês Soares <up201606615@fe.up.pt>
2020-12-16 18:01:25 +0000

    qr code reader is reading the code

--- app/src/main/java/pt/atp/app_seai_g/FragmentCharge.kt
+++ app/src/main/java/pt/atp/app_seai_g/FragmentCharge.kt
@@ -9,6 +9,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureActivity
 
 
 class FragmentCharge : Fragment(R.layout.fragment_charge) {
@@ -17,7 +19,7 @@ class FragmentCharge : Fragment(R.layout.fragment_charge) {
 
         val rootView: View = inflater.inflate(R.layout.fragment_charge,container,false)
         val buttonSendId: Button = rootView.findViewById(R.id.button_sendid)
-//        val buttonQrCode: Button = rootView.findViewById(R.id.button_qrcode)
+        val buttonQrCode: Button = rootView.findViewById(R.id.button_qrcode)
 //        val buttonNFC: Button = rootView.findViewById(R.id.button_nfc)
 
         buttonSendId.setOnClickListener {
@@ -33,7 +35,7 @@ class FragmentCharge : Fragment(R.layout.fragment_charge) {
 
         }
 
-/*        buttonQrCode.setOnClickListener{
+        buttonQrCode.setOnClickListener{
             val integrator = IntentIntegrator(activity).apply{
                 captureActivity= CaptureActivity::class.java
                 setOrientationLocked(false)
@@ -43,7 +45,7 @@ class FragmentCharge : Fragment(R.layout.fragment_charge) {
             integrator.initiateScan()
         }
 
-        buttonNFC.setOnClickListener{
+        /*buttonNFC.setOnClickListener{
             val intent = Intent(context, ReceiverActivity::class.java)
             startActivity(intent)
         }*/
