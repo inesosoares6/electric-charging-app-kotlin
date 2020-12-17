06403656086d196e3895bb6cc0d8bc3d852b35cf
Inês Soares <up201606615@fe.up.pt>
2020-12-17 09:21:44 +0000

    optimize code

--- app/src/main/java/pt/atp/app_seai_g/FragmentCharge.kt
+++ app/src/main/java/pt/atp/app_seai_g/FragmentCharge.kt
@@ -2,23 +2,19 @@ package pt.atp.app_seai_g
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.SyncStateContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.android.gms.common.internal.Constants
 import com.google.zxing.integration.android.IntentIntegrator
-import com.journeyapps.barcodescanner.CaptureActivity
 
 
 class FragmentCharge : Fragment(R.layout.fragment_charge) {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         val rootView: View = inflater.inflate(R.layout.fragment_charge,container,false)
         val buttonSendId: Button = rootView.findViewById(R.id.button_sendid)
@@ -26,14 +22,8 @@ class FragmentCharge : Fragment(R.layout.fragment_charge) {
 //        val buttonNFC: Button = rootView.findViewById(R.id.button_nfc)
 
         buttonSendId.setOnClickListener {
-
             val chargerIdText: EditText = rootView.findViewById(R.id.idcarregador)
-            val list = listOf("202001","202002","202003","202004","202005")
-            if(list.contains(chargerIdText.text.toString())){
-                sendID(chargerIdText.text.toString())
-            } else{
-                Toast.makeText(context, getString(R.string.insertValidId),Toast.LENGTH_LONG).show()
-            }
+            confirmIdCharger(chargerIdText.text.toString())
         }
 
         buttonQrCode.setOnClickListener{
@@ -55,18 +45,22 @@ class FragmentCharge : Fragment(R.layout.fragment_charge) {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        val intent = Intent(context,ActivityCharging::class.java)
         if (result != null) {
             if (result.contents == null){
                 Toast.makeText(context, "Cancelled", Toast.LENGTH_LONG).show()
             }
             else{
                 Toast.makeText(context, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-                sendID(result.contents)
+                confirmIdCharger(result.contents)
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    private fun confirmIdCharger(chargerID: String){
+        //TODO verify if id charger is ready to use (value available in database)
+        sendID(chargerID)
     }
 
     private fun sendID(chargerID: String){
@@ -74,5 +68,4 @@ class FragmentCharge : Fragment(R.layout.fragment_charge) {
         intent.putExtra("chargerID",chargerID)
         startActivity(intent)
     }
-
 }
