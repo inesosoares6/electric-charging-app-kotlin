85e378c8bc4b3941b7c781019c4bb5b7b5a78d19
Inês Soares <up201606615@fe.up.pt>
2020-12-17 10:24:38 +0000

    Transformed login activity to viewModel

--- app/src/main/java/pt/atp/app_seai_g/ActivityLogin.kt
+++ app/src/main/java/pt/atp/app_seai_g/ActivityLogin.kt
@@ -3,16 +3,16 @@ package pt.atp.app_seai_g
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.Gravity
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
 class ActivityLogin : AppCompatActivity() {
+
+    private val viewModel: ViewModelLogin by viewModels()
 
     private var mAuth: FirebaseAuth? = null
     private var emailTV: EditText? = null
@@ -23,20 +23,32 @@ class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setup()
+    }
 
+    private fun setup() {
         mAuth = FirebaseAuth.getInstance()
         initializeUI()
-        loginBtn!!.setOnClickListener { loginUserAccount() }
+        loginBtn!!.setOnClickListener { validateCredentialsAndRedirect() }
         registerBtn!!.setOnClickListener{
             val intent = Intent(this, ActivityRegister::class.java)
             startActivity(intent)
         }
+        viewModel.loginResultLiveData.observe(this){ loginResult ->
+            if(!loginResult){
+                Toast.makeText(applicationContext, getString(R.string.failLogin), Toast.LENGTH_LONG).show()
+            } else{
+                Toast.makeText(applicationContext, getString(R.string.successLogin), Toast.LENGTH_LONG).show()
+                val intent = Intent(this, ActivityWelcome::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 
-    private fun loginUserAccount() {
+    private fun validateCredentialsAndRedirect(){
         val email: String = emailTV!!.text.toString()
         val password: String = passwordTV!!.text.toString()
-
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(applicationContext, getString(R.string.enterEmail), Toast.LENGTH_LONG).show()
             return
@@ -45,16 +57,7 @@ class ActivityLogin : AppCompatActivity() {
             Toast.makeText(applicationContext, getString(R.string.enterPassword), Toast.LENGTH_LONG).show()
             return
         }
-        mAuth!!.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(applicationContext, getString(R.string.successLogin), Toast.LENGTH_LONG).show()
-                    val intent = Intent(this, ActivityWelcome::class.java)
-                    startActivity(intent)
-                } else {
-                    Toast.makeText(applicationContext, getString(R.string.failLogin), Toast.LENGTH_LONG).show()
-                }
-            }
+        viewModel.areCredentialsValid(mAuth,email,password)
     }
 
     private fun initializeUI() {
---
+++ app/src/main/java/pt/atp/app_seai_g/ViewModelLogin.kt
@@ -0,0 +1,22 @@
+package pt.atp.app_seai_g
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class ViewModelLogin : ViewModel() {
+
+    private val _loginResult = MutableLiveData<Boolean>()
+    val loginResultLiveData = _loginResult
+
+    fun areCredentialsValid(mAuth: FirebaseAuth?, email: String, password: String){
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    loginResultLiveData.postValue(true)
+                } else {
+                    loginResultLiveData.postValue(false)
+                }
+            }
+    }
+}
