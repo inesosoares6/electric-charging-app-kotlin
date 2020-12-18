8bb99b1805a75243b4134e9fc68960797ecfcb5e
Inês Soares <up201606615@fe.up.pt>
2020-12-18 17:05:09 +0000

    Able to read changes in SwitchPreferenceCompact button

--- app/src/main/java/pt/atp/app_seai_g/SettingsActivity.kt
+++ app/src/main/java/pt/atp/app_seai_g/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package pt.atp.app_seai_g
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 
 // Activity of settings
 
@@ -18,8 +20,45 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
+        private var preferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener? = null
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            val onOffRandomColor: SwitchPreferenceCompat? = findPreference("darkTheme")
+            //val onOffRandomColor = findPreference("darkTheme") as SwitchPreference?
+
+            // SwitchPreference preference change listener
+            onOffRandomColor!!.setOnPreferenceChangeListener { _, _ ->
+                if (onOffRandomColor.isChecked) {
+                    Toast.makeText(context, "Unchecked", Toast.LENGTH_SHORT).show()
+
+                    // Checked the switch programmatically
+                    onOffRandomColor.isChecked = false
+                } else {
+                    Toast.makeText(context, "Checked", Toast.LENGTH_SHORT).show()
+
+                    // Unchecked the switch programmatically
+                    onOffRandomColor.isChecked = true
+                }
+                false
+            }
+            /*preferenceChangeListener =
+                SharedPreferences.OnSharedPreferenceChangeListener { _, key ->
+                    when (val preference = findPreference<PreferenceCategory>(key)) {
+                        is SwitchPreferenceCompat -> {
+                            if (key == "darkTheme") {
+                                Toast.makeText(context, "darkTheme : " + preference.isChecked, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                        is CheckBoxPreference -> {
+                            // do sth else
+                        }
+                        is ListPreference -> {
+                            // do sth else
+                        }
+                    }
+                }*/
         }
     }
 }
