9fc7d46f36b0b6a23dceeab89a77ca39065378cf
Inês Soares <up201606615@fe.up.pt>
2020-12-18 11:21:25 +0000

    Created map activity

--- app/src/main/java/pt/atp/app_seai_g/FragmentHome.kt
+++ app/src/main/java/pt/atp/app_seai_g/FragmentHome.kt
@@ -1,7 +1,36 @@
 package pt.atp.app_seai_g
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 
 
 class FragmentHome : Fragment(R.layout.fragment_home) {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView: View = inflater.inflate(R.layout.fragment_home,container,false)
+        val mapButton: Button = rootView.findViewById(R.id.mapButton)
+
+        mapButton.setOnClickListener{
+            val intent = Intent(context, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Gets the map from the xml layout and creates it
+        /*val mapView: MapView  = rootView.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+
+        // Gets GoogleMap from the MapView and does initialization
+        val map: GoogleMap = mapView.getMapAsync(this)*/
+
+        return rootView
+    }
+
+
+
 }
