6d72d35cb32885222aa17f2913073bfb71ea5576
Inês Soares <up201606615@fe.up.pt>
2020-12-21 14:27:27 +0000

    AddVehicle activity setup (TODO: put values in database)

--- app/src/main/java/pt/atp/app_seai_g/AddVehicleActivity.kt
+++ app/src/main/java/pt/atp/app_seai_g/AddVehicleActivity.kt
@@ -1,9 +1,12 @@
 package pt.atp.app_seai_g
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class AddVehicleActivity : AppCompatActivity() {
 
@@ -11,15 +14,16 @@ class AddVehicleActivity : AppCompatActivity() {
     private var brandTV: EditText? = null
     private var modelTV: EditText? = null
     private var buttonAddVehicle: Button? = null
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_vehicle)
         initializeUI()
+        database = Firebase.database.reference
 
         buttonAddVehicle!!.setOnClickListener{
-            //TODO verify info of vehicle
-
+            //TODO verify info of vehicle and write to database
         }
 
     }
