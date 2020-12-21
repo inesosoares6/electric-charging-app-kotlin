81c7ac74c51d44534a461d81ba3aee1f9cb75e75
Inês Soares <up201606615@fe.up.pt>
2020-12-21 11:47:17 +0000

    Updated class Vehicle

--- app/src/main/java/pt/atp/app_seai_g/models/Vehicle.kt
+++ app/src/main/java/pt/atp/app_seai_g/models/Vehicle.kt
@@ -1,12 +1,25 @@
 package pt.atp.app_seai_g.models
 
+import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
 
 // [START blog_user_class]
 @IgnoreExtraProperties
 data class Vehicle(
+    var uid: String? = "",
     var numCharges: Int = 0,
     var type: String? = "",
     var brand: String? = "",
     var model: String? = "",
-)
+) {
+    @Exclude
+    fun toMap(): Map<String,Any?>{
+        return mapOf(
+            "uid" to uid,
+            "numCharges" to numCharges,
+            "type" to type,
+            "brand" to brand,
+            "model" to model
+        )
+    }
+}
