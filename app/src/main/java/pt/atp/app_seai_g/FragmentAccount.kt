dfce2819c2880c93893318c7fa25b7dbc2fac0b7
Inês Soares <up201606615@fe.up.pt>
2020-12-22 19:41:00 +0000

    Added number of charges in account fragment

--- app/src/main/java/pt/atp/app_seai_g/FragmentAccount.kt
+++ app/src/main/java/pt/atp/app_seai_g/FragmentAccount.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
@@ -29,17 +30,20 @@ class FragmentAccount : Fragment(R.layout.fragment_account) {
 
     private var fbAuth = FirebaseAuth.getInstance()
 
-    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    @SuppressLint("UseSwitchCompatOrMaterialCode", "SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val rootView: View = inflater.inflate(R.layout.fragment_account,container,false)
         val buttonLogout: Button = rootView.findViewById(R.id.logoutButton)
         val settingsButton: FloatingActionButton = rootView.findViewById(R.id.settingsButton)
         val darkThemeButton: Switch = rootView.findViewById(R.id.darkThemeButton)
         val historicButton: FloatingActionButton = rootView.findViewById(R.id.historicButton)
+        val numChargesText: TextView = rootView.findViewById(R.id.numChargesText)
         val database = Firebase.database
         val myRef = database.getReference("message")
         // Access a Cloud Firestore instance from your Activity
         val db = FirebaseFirestore.getInstance()
+        val mAuth: FirebaseAuth?
+        mAuth=FirebaseAuth.getInstance()
 
         buttonLogout.setOnClickListener{
             fbAuth.signOut()
@@ -57,6 +61,18 @@ class FragmentAccount : Fragment(R.layout.fragment_account) {
         settingsButton.setOnClickListener{
             val intent = Intent(context,SettingsActivity::class.java)
             startActivity(intent)
+        }
+
+        var numCharges : String
+        mAuth.currentUser?.email?.let {
+            db.collection("users").document(it).get()
+                    .addOnSuccessListener { result ->
+                        numCharges = result["numCharges"].toString()
+                        numChargesText.text = getString(R.string.number_of_charges) + "   " + numCharges
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(context,"Error in reading number of charges",Toast.LENGTH_LONG).show()
+                    }
         }
 
         historicButton.setOnClickListener{
