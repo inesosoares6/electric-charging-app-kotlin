d0b3e0047564951417fc09037a71295aa8e57ae2
Inês Soares <up201606615@fe.up.pt>
2020-12-26 18:19:03 +0000

    Configured pop-up in vehicle list

--- app/src/main/java/pt/atp/app_seai_g/ActivityVehicleList.kt
+++ app/src/main/java/pt/atp/app_seai_g/ActivityVehicleList.kt
@@ -1,28 +1,35 @@
 package pt.atp.app_seai_g
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import pt.atp.app_seai_g.models.VehicleAdapter
 
 class ActivityVehicleList : AppCompatActivity() {
+
+    // Access a Cloud Firestore instance from your Activity
+    private val db = FirebaseFirestore.getInstance()
+    private var mAuth: FirebaseAuth? = null
+    private val arrayType: ArrayList<String> = ArrayList()
+    private val arrayDetails: ArrayList<String> = ArrayList()
+    private val arrayDocs: ArrayList<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vehicle_list)
-        // Access a Cloud Firestore instance from your Activity
-        val db = FirebaseFirestore.getInstance()
-        val mAuth: FirebaseAuth?
         mAuth=FirebaseAuth.getInstance()
-        val arrayType: ArrayList<String> = ArrayList()
-        val arrayDetails: ArrayList<String> = ArrayList()
 
-        mAuth.currentUser?.email?.let {
+        mAuth!!.currentUser?.email?.let {
             db.collection("users").document(it).collection("vehicles").get()
                 .addOnSuccessListener { result ->
                     for(document in result){
+                        arrayDocs.add(document.id)
                         arrayType.add(document["type"].toString())
                         arrayDetails.add((document["brand"].toString())+" - "+(document["model"] as String))
                     }
@@ -42,7 +49,48 @@ class ActivityVehicleList : AppCompatActivity() {
         listView.setOnItemClickListener { adapterView, _, position, _ ->
             val itemAtPos = adapterView.getItemAtPosition(position)
             val itemIdAtPos = adapterView.getItemIdAtPosition(position)
-            Toast.makeText(this, "Click on item at $itemAtPos its item id $itemIdAtPos", Toast.LENGTH_LONG).show()
+            //Toast.makeText(this, "Click on item at $itemAtPos its item id $itemIdAtPos", Toast.LENGTH_LONG).show()
+
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setMessage(getString(R.string.performAction))
+                    .setCancelable(false)
+                    .setPositiveButton(getString(R.string.favorite), DialogInterface.OnClickListener { dialog, id ->
+                        //TODO set as favorite
+                        //dialog.cancel()
+                        setAsFavorite(itemIdAtPos.toInt())
+                    })
+                    .setNegativeButton(getString(R.string.deleteFromList), DialogInterface.OnClickListener { _, _ ->
+                        deleteFromList(itemIdAtPos.toInt())
+                    })
+                    .setNeutralButton(getString(R.string.cancelElimination),DialogInterface.OnClickListener { dialog, _ ->
+                        dialog.cancel()
+                    })
+            val alert = dialogBuilder.create()
+            alert.setTitle(getString(R.string.vehicle) + " " + (itemIdAtPos+1))
+            alert.show()
+        }
+    }
+
+    private fun deleteFromList(idList: Int) {
+        mAuth?.currentUser?.email?.let {
+            db.collection("users").document(it).collection("vehicles").document(arrayDocs[idList]).delete()
+        }
+        finish()
+    }
+
+    private fun setAsFavorite(idList: Int) {
+        val favVehicle = hashMapOf(
+                "type" to arrayType[idList],
+                "details" to arrayDetails[idList]
+        )
+        mAuth?.currentUser?.email?.let {
+            db.collection("users").document(it).collection("favVehicle").document(arrayDocs[idList]).set(favVehicle)
+                    .addOnSuccessListener {
+                        Toast.makeText(applicationContext, getString(R.string.vehicle_addition_successful), Toast.LENGTH_LONG).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(applicationContext, getString(R.string.error_adding_vehicle), Toast.LENGTH_LONG).show()
+                    }
         }
     }
 }
