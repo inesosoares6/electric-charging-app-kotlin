fb66e10064ee9c4725f966ef3268922d9a464fa5
Inês Soares <up201606615@fe.up.pt>
2020-12-26 18:51:51 +0000

    Optimized imports

--- app/src/main/java/pt/atp/app_seai_g/ActivityDeleteUser.kt
+++ app/src/main/java/pt/atp/app_seai_g/ActivityDeleteUser.kt
@@ -1,11 +1,10 @@
 package pt.atp.app_seai_g
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
